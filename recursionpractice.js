b669db0b1f4bb30d831cde48af39901d7354fb9b
Brian Gerson <brianpgerson@gmail.com>
2015-08-31 23:45:44 -0700

    finally made some recursive understanding headway. return the value itself, not a variable for the value

--- recursionpractice.js
+++ recursionpractice.js
@@ -63,3 +63,27 @@ function explode(str){
 	}
 }
 
+function ind(el, list){
+	var not = [];
+	if (list[0] == [] || list[0] == undefined) {
+		console.log("not in here.")
+		return 1;
+	}
+	else if (el == list[0]){
+		return 0;
+	} else {
+		return not.push(list[0]) + ind(el, list.slice(1));
+	}
+}
+
+function ind(el, list){
+	if (list[0] == [] || list[0] == undefined) {
+		console.log("not in here.")
+		return 1;
+	}
+	else if (el == list[0]){
+		return 0;
+	} else {
+		return 1 + ind(el, list.slice(1));
+	}
+}
